3e3a00413fd895b51cbb436636e9042b9dae2d3c
Luis Gonzalez <162064187+LuisJG8@users.noreply.github.com>
2024-08-29 20:30:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
+date = input("What year would you like to travel to? Type the date in this format YYYY-MM-DD: ")
 
-# year = input("What year would you like to travel to? Type the date in this format YYYY-MM-DD: ")
-
-connection = requests.get(f"https://www.billboard.com/charts/hot-100//")
+time.sleep(2)
+connection = requests.get(f"https://www.billboard.com/charts/hot-100/{date}/")
 connection_text = connection.text
 print(connection.raise_for_status())
 
 soup = BeautifulSoup(connection_text, "html.parser")
 
-all_titles = soup.find(name="li", class_="lrv-u-width-100p")
-print(all_titles)
+# first_title = soup.find_all(name="h3", class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only")
+# all_titles = soup.find_all(name="h3", class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only")
+# print(all_titles)
 
-# 2001-08-12
+names = soup.select("li ul li h3")
+# print(names)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+new_song_list = [text.getText().strip() for text in names]
+print(new_song_list)
